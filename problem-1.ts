37622f261b8f71436f4ba3cfb7f97322e286c518
AnikHaque <ekramul15-811@diu.edu.bd>
2023-08-05 15:46:14 +0600

    problem-1 done

---
+++ problem-1.ts
@@ -0,0 +1,7 @@
+function firstProblem(str: string, num: number = 3): void {
+    for (let i = 0; i < num; i++) {
+      console.log(str);
+    }
+  }
+  firstProblem("Programming"); 
+  firstProblem("Hero", 4); 
